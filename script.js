72e403d2043eb943ccc2f585b85d96e45d5ca62e
Ann-Fox <fx8565@mail.ru>
2025-05-11 17:13:36 +0500

    feat: отображекние детальной информации о товаре

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const bodyTableElement = document.querySelector("tbody");
+const resultElement = document.querySelector(".result");
 
+// Загрузка списка товаров через REST API
 fetch("https://fakestoreapi.com/products")
     .then((response) => {
         console.log("response:", response);
         return response.json();
     })
     .then((data) => {
-        console.log(data);
+        // console.log(data);
         // заполнить body таблицы
         bodyTableElement.innerHTML = data
             .map(
                 ({ id, title, price }) =>
-                    ` <tr>
+                    ` <tr class="item-product">
                         <td>${id}</td>
                         <td>${title}</td>
                         <td>${price}</td>
                     </tr>`
             )
             .join("");
+
+        // При клике на строку в таблице отображается детальная информация о выбранном товаре
+        const trElements = document.querySelectorAll(".item-product");
+
+        trElements.forEach((el, index) => {
+            console.log(el, index);
+            el.addEventListener("click", function () {
+                resultElement.innerHTML = `
+                <img src="${data[index].image}">
+                <p>${data[index].title}</p>
+                <p>${data[index].price}</p>`;
+            });
+        });
     });
